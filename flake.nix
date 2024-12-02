bf6df1ecce2b8334ba8d6d40bd1b98513cf91527
Uli Luckas <luckas@musoft.de>
2024-12-02 09:35:44 +0100

    Follow default namespace rules

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
   description = "Nix configurations for custom packages";
 
-  outputs = { self, nixpkgs, ... }: {
-    packages.x86_64-linux.lookbusy = import ./lookbusy/default.nix;
-    packages.aarch64-linux.lookbusy = import ./lookbusy/default.nix;
-    packages.armv7l-linux.lookbusy = import ./lookbusy/default.nix;
-    packages.armv6l-linux.lookbusy = import ./lookbusy/default.nix;
+  outputs = { self, nixpkgs }: {
+    packages = {
+      default = { system }: let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          lookbusy = pkgs.callPackage ./lookbusy.nix { };
+        };
+    };
   };
-}
