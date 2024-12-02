d603e993c74518e682c7adba94373926ef26237c
Uli Luckas <luckas@musoft.de>
2024-12-02 09:28:20 +0100

    Add arm architectures

--- flake.nix
+++ flake.nix
@@ -3,5 +3,8 @@
 
   outputs = { self, nixpkgs, ... }: {
     packages.x86_64-linux.lookbusy = import ./lookbusy/default.nix;
+    packages.aarch64-linux.lookbusy = import ./lookbusy/default.nix;
+    packages.armv7l-linux.lookbusy = import ./lookbusy/default.nix;
+    packages.armv6l-linux.lookbusy = import ./lookbusy/default.nix;
   };
 }
