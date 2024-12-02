3181103330c7a8d2881fb4c786b28e97aadef6c3
Uli Luckas <luckas@musoft.de>
2024-12-02 09:59:28 +0100

    Dynamically support multiple architectures in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,12 +2,18 @@
   description = "Nix configurations for custom packages";
 
   outputs = { self, nixpkgs }: {
-    packages = {
-      default = { system }: let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "armv7l-linux" "armv6l-linux" ];
+    legacyPackages = builtins.genAttrs supportedSystems (system:
+      import nixpkgs { inherit system; }
+    );
+
+    # Dynamically support architectures for packages
+    packages.default = builtins.genAttrs supportedSystems (system:
+      let
         pkgs = import nixpkgs { inherit system; };
-      in
-        {
-          lookbusy = pkgs.callPackage ./lookbusy.nix { };
-        };
-    };
+      in {
+        lookbusy = pkgs.callPackage ./lookbusy.nix { };
+      }
+    );
   };
+}
